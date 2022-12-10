a3784904fc8a6fbde7023f2436ff89363f79d901
Nikola <118454864+Niukuu@users.noreply.github.com>
2022-12-10 21:02:06 +0100

    Add files via upload

---
+++ CountdownWithVoice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace CountdownWithVoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer s = new SpeechSynthesizer();
+            int count = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i <= count; i++)
+            {
+                Console.WriteLine(i);
+                string yes = Convert.ToString(i);
+                s.Speak(yes);
+            }
+            Console.ReadKey();
+        }
+    }
+}
